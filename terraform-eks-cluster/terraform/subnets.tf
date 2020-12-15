5b84dd9456aaed188b8c86820f576eb8f5d0a4be
Anton Putra <antonputra@icloud.com>
2020-12-15 15:08:15 -0800

    Add subnets

---
+++ terraform-eks-cluster/terraform/subnets.tf
@@ -0,0 +1,80 @@
+# Resource: aws_subnet
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+
+resource "aws_subnet" "public_1" {
+  # The VPC ID.
+  vpc_id = aws_vpc.main.id
+
+  # The CIDR block for the subnet.
+  cidr_block = "192.168.0.0/18"
+
+  # The AZ for the subnet.
+  availability_zone = "us-east-1a"
+
+  # Required for EKS. Instances launched into the subnet should be assigned a public IP address.
+  map_public_ip_on_launch = true
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name                        = "public-us-east-1a"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  # The VPC ID
+  vpc_id = aws_vpc.main.id
+
+  # The CIDR block for the subnet.
+  cidr_block = "192.168.64.0/18"
+
+  # The AZ for the subnet.
+  availability_zone = "us-east-1b"
+
+  # Required for EKS. Instances launched into the subnet should be assigned a public IP address.
+  map_public_ip_on_launch = true
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name                        = "public-us-east-1b"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  # The VPC ID
+  vpc_id = aws_vpc.main.id
+
+  # The CIDR block for the subnet.
+  cidr_block = "192.168.128.0/18"
+
+  # The AZ for the subnet.
+  availability_zone = "us-east-1a"
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name                              = "private-us-east-1a"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  # The VPC ID
+  vpc_id = aws_vpc.main.id
+
+  # The CIDR block for the subnet.
+  cidr_block = "192.168.192.0/18"
+
+  # The AZ for the subnet.
+  availability_zone = "us-east-1b"
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name                              = "private-us-east-1b"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
