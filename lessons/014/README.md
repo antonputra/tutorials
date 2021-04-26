d620446840c797825446425c57a0ce37998da213
Anton Putra <antonputra@icloud.com>
2021-04-26 15:40:35 +0500

    add 014

---
+++ lessons/014/README.md
@@ -0,0 +1,3 @@
+# Terraform Ansible Integration | Terraform Ansible AWS Example?
+
+[Step by Step Tutorial](https://youtu.be/QxgJlJgGA0E)
---
+++ lessons/014/main.tf
@@ -0,0 +1,64 @@
+locals {
+  vpc_id           = "vpc-0548d408bf3549ca0"
+  subnet_id        = "subnet-060a1ae52cf0a73d6"
+  ssh_user         = "ubuntu"
+  key_name         = "devops"
+  private_key_path = "~/Downloads/devops.pem"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "nginx" {
+  name   = "nginx_access"
+  vpc_id = local.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami                         = "ami-0dba2cb6798deb6d8"
+  subnet_id                   = "subnet-060a1ae52cf0a73d6"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.nginx.id]
+  key_name                    = local.key_name
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = aws_instance.nginx.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook  -i ${aws_instance.nginx.public_ip}, --private-key ${local.private_key_path} nginx.yaml"
+  }
+}
+
+output "nginx_ip" {
+  value = aws_instance.nginx.public_ip
+}
