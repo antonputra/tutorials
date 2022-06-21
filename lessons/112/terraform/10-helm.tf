e53d2a310a598b927af8fad0dc6253bca7660b5f
Anton Putra <me@antonputra.com>
2022-06-21 17:30:00 +0500

    update helm api version (#75)

--- lessons/112/terraform/10-helm.tf
+++ lessons/112/terraform/10-helm.tf
@@ -3,7 +3,7 @@ provider "helm" {
     host                   = aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.id]
       command     = "aws"
     }
