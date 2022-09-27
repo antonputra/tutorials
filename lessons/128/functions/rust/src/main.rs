17d5efa337052983ba99af29b007605ead8f86ec
Thomas Sieverding <Thomas@bajix.com>
2022-09-26 21:33:59 -0700

    Make more idiomatic

--- lessons/128/functions/rust/src/main.rs
+++ lessons/128/functions/rust/src/main.rs
@@ -1,13 +1,27 @@
 use aws_config::SdkConfig;
-use aws_sdk_dynamodb::model::AttributeValue;
-use aws_sdk_s3::types::DateTime;
+use aws_sdk_dynamodb::{error::PutItemError, model::AttributeValue, types::SdkError};
+use aws_sdk_s3::error::GetObjectError;
+use aws_smithy_http::byte_stream;
 use aws_smithy_types_convert::date_time::DateTimeExt;
-use chrono::Duration as OldDuration;
-use chrono::Utc;
-use lambda_http::{run, service_fn, Body, Error, Request, Response};
+use chrono::{Duration, Utc};
+use lambda_http::{run, service_fn, Body, Request, Response};
 use rand::Rng;
 use std::env;
-use std::time::Duration;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    PutItem(#[from] SdkError<PutItemError>),
+    #[error(transparent)]
+    GetObject(#[from] SdkError<GetObjectError>),
+    #[error(transparent)]
+    ByteStream(#[from] byte_stream::Error),
+    #[error(transparent)]
+    Http(#[from] lambda_http::http::Error),
+    #[error(transparent)]
+    Lambda(#[from] lambda_http::Error),
+}
 
 async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
     let bucket = env::var("BUCKET_NAME").expect("failed to get env");
@@ -15,16 +29,16 @@ async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
     let table_name = "images";
     let config = aws_config::load_from_env().await;
 
-    let date = get_s3_object(&bucket, &key, &config).await;
-    let date = get_new_date(date);
-
-    save_last_modified(&table_name, &date.to_string(), &config).await;
+    if let Some(modified) = get_s3_object(&bucket, &key, &config).await? {
+        let date = get_new_date(modified);
+        save_last_modified(&table_name, &date.to_string(), &config).await?;
+    }
 
     let resp = Response::builder()
         .status(200)
         .header("content-type", "application/json")
-        .body("OK".into())
-        .map_err(Box::new)?;
+        .body("OK".into())?;
+
     Ok(resp)
 }
 
@@ -36,30 +50,34 @@ async fn main() -> Result<(), Error> {
         .without_time()
         .init();
 
-    run(service_fn(function_handler)).await
+    run(service_fn(function_handler)).await?;
+
+    Ok(())
 }
 
 // Downloads S3 object and returns last modified date in UTC format.
-async fn get_s3_object(bucket: &str, key: &str, config: &SdkConfig) -> chrono::DateTime<Utc> {
+async fn get_s3_object(
+    bucket: &str,
+    key: &str,
+    config: &SdkConfig,
+) -> Result<Option<chrono::DateTime<Utc>>, Error> {
     let client = aws_sdk_s3::Client::new(config);
 
-    let object = client
-        .get_object()
-        .bucket(bucket)
-        .key(key)
-        .send()
-        .await
-        .expect("failed to fetch S3 object");
+    let object = client.get_object().bucket(bucket).key(key).send().await?;
 
-    let date = object.last_modified().expect("failed to get date");
-    let date: DateTime = date.clone().into();
-
-    object.body.collect().await.expect("failed to read body");
-    date.to_chrono_utc()
+    if let Some(last_modified) = object.last_modified() {
+        Ok(Some(last_modified.to_chrono_utc()))
+    } else {
+        Ok(None)
+    }
 }
 
 // Saves the last modified date to the DynamoDB table.
-async fn save_last_modified(table_name: &str, date: &str, config: &SdkConfig) {
+async fn save_last_modified(
+    table_name: &str,
+    date: &str,
+    config: &SdkConfig,
+) -> Result<(), SdkError<PutItemError>> {
     let client = aws_sdk_dynamodb::Client::new(config);
     let date = AttributeValue::S(date.into());
 
@@ -68,15 +86,14 @@ async fn save_last_modified(table_name: &str, date: &str, config: &SdkConfig) {
         .table_name(table_name)
         .item("last_modified_date", date)
         .send()
-        .await
-        .expect("failed to sate item to dynamodb");
+        .await?;
+
+    Ok(())
 }
 
 // Generates new random date.
 fn get_new_date(date: chrono::DateTime<Utc>) -> chrono::DateTime<Utc> {
     let random_number_of_days = rand::thread_rng().gen_range(1..=10000) * 60 * 60 * 24;
-
-    let duration = Duration::new(random_number_of_days, 0);
-    let duration = OldDuration::from_std(duration).expect("failed");
-    date.checked_add_signed(duration).expect("failed")
+    let duration = Duration::days(random_number_of_days);
+    date.checked_add_signed(duration).unwrap()
 }
