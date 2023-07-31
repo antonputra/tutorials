9323735e95754b71fe7508c4096c6e8904b9b9dc
Vladimir Kuletski <vladimir.kuletski@gmail.com>
2023-07-31 16:55:11 -0400

    fmt

--- lessons/150/terraform/13-private-dns.tf
+++ lessons/150/terraform/13-private-dns.tf
@@ -1,5 +1,5 @@
 locals {
-  private_route53_zone  = "antonputra.pvt"
+  private_route53_zone = "antonputra.pvt"
 }
 
 resource "aws_route53_zone" "private" {
@@ -12,7 +12,7 @@ resource "aws_route53_zone" "private" {
 
 #arm64
 provider "kubernetes" {
-  alias = "k8s-arm64"
+  alias                  = "k8s-arm64"
   host                   = aws_eks_cluster.demo_arm64.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.demo_arm64.certificate_authority[0].data)
   exec {
@@ -25,24 +25,24 @@ provider "kubernetes" {
 data "kubernetes_service" "internal-ingress-arm64" {
   provider = kubernetes.k8s-arm64
   metadata {
-    name = "internal-ingress-nginx-controller"
+    name      = "internal-ingress-nginx-controller"
     namespace = "ingress-nginx"
   }
   depends_on = [helm_release.internal_ingress_nginx_arm64]
 }
 
 resource "aws_route53_record" "api_arm64" {
-  zone_id = aws_route53_zone.private.zone_id
-  name    = "api.arm64"
-  type    = "CNAME"
-  ttl     = 300
-  records = [data.kubernetes_service.internal-ingress-arm64.status.0.load_balancer.0.ingress.0.hostname]
+  zone_id    = aws_route53_zone.private.zone_id
+  name       = "api.arm64"
+  type       = "CNAME"
+  ttl        = 300
+  records    = [data.kubernetes_service.internal-ingress-arm64.status.0.load_balancer.0.ingress.0.hostname]
   depends_on = [helm_release.internal_ingress_nginx_arm64]
 }
 
 #amd64
 provider "kubernetes" {
-  alias = "k8s-amd64"
+  alias                  = "k8s-amd64"
   host                   = aws_eks_cluster.demo_amd64.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.demo_amd64.certificate_authority[0].data)
   exec {
@@ -55,17 +55,17 @@ provider "kubernetes" {
 data "kubernetes_service" "internal-ingress-amd64" {
   provider = kubernetes.k8s-amd64
   metadata {
-    name = "internal-ingress-nginx-controller"
+    name      = "internal-ingress-nginx-controller"
     namespace = "ingress-nginx"
   }
   depends_on = [helm_release.internal_ingress_nginx_amd64]
 }
 
 resource "aws_route53_record" "api_amd64" {
-  zone_id = aws_route53_zone.private.zone_id
-  name    = "api.amd64"
-  type    = "CNAME"
-  ttl     = 300
-  records = [data.kubernetes_service.internal-ingress-amd64.status.0.load_balancer.0.ingress.0.hostname]
+  zone_id    = aws_route53_zone.private.zone_id
+  name       = "api.amd64"
+  type       = "CNAME"
+  ttl        = 300
+  records    = [data.kubernetes_service.internal-ingress-amd64.status.0.load_balancer.0.ingress.0.hostname]
   depends_on = [helm_release.internal_ingress_nginx_amd64]
 }
--- lessons/150/terraform/14-public-dns.tf
+++ lessons/150/terraform/14-public-dns.tf
@@ -1,5 +1,5 @@
 locals {
-  public_route53_zone  = "antonputra.com"
+  public_route53_zone = "antonputra.com"
 }
 
 data "aws_route53_zone" "public" {
@@ -12,17 +12,17 @@ data "aws_route53_zone" "public" {
 data "kubernetes_service" "external-ingress-arm64" {
   provider = kubernetes.k8s-arm64
   metadata {
-    name = "external-ingress-nginx-controller"
+    name      = "external-ingress-nginx-controller"
     namespace = "ingress-nginx"
   }
   depends_on = [helm_release.external_ingress_nginx_arm64]
 }
 resource "aws_route53_record" "arm64_prometheus" {
-  zone_id = data.aws_route53_zone.public.zone_id
-  name    = "prometheus.arm64"
-  type    = "CNAME"
-  ttl     = 300
-  records = [data.kubernetes_service.external-ingress-arm64.status.0.load_balancer.0.ingress.0.hostname]
+  zone_id    = data.aws_route53_zone.public.zone_id
+  name       = "prometheus.arm64"
+  type       = "CNAME"
+  ttl        = 300
+  records    = [data.kubernetes_service.external-ingress-arm64.status.0.load_balancer.0.ingress.0.hostname]
   depends_on = [helm_release.external_ingress_nginx_arm64]
 }
 
@@ -30,16 +30,16 @@ resource "aws_route53_record" "arm64_prometheus" {
 data "kubernetes_service" "external-ingress-amd64" {
   provider = kubernetes.k8s-amd64
   metadata {
-    name = "external-ingress-nginx-controller"
+    name      = "external-ingress-nginx-controller"
     namespace = "ingress-nginx"
   }
   depends_on = [helm_release.external_ingress_nginx_amd64]
 }
 resource "aws_route53_record" "amd64_prometheus" {
-  zone_id = data.aws_route53_zone.public.zone_id
-  name    = "prometheus.amd64"
-  type    = "CNAME"
-  ttl     = 300
-  records = [data.kubernetes_service.external-ingress-amd64.status.0.load_balancer.0.ingress.0.hostname]
+  zone_id    = data.aws_route53_zone.public.zone_id
+  name       = "prometheus.amd64"
+  type       = "CNAME"
+  ttl        = 300
+  records    = [data.kubernetes_service.external-ingress-amd64.status.0.load_balancer.0.ingress.0.hostname]
   depends_on = [helm_release.external_ingress_nginx_amd64]
 }
