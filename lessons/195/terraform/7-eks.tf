d51910561a1ec1d5a377a41167edb89fc28faa83
Anton Putra <me@antonputra.com>
2024-05-20 16:25:19 +0500

    Create AWS EKS Cluster using Terraform: AWS EKS Kubernetes Tutorial - Part 2 (#230)

---
+++ lessons/195/terraform/7-eks.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "eks" {
+  name = "${local.env}-${local.eks_name}-eks-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      }
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks.name
+}
+
+resource "aws_eks_cluster" "eks" {
+  name     = "${local.env}-${local.eks_name}"
+  version  = local.eks_version
+  role_arn = aws_iam_role.eks.arn
+
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access  = true
+
+    subnet_ids = [
+      aws_subnet.private_zone1.id,
+      aws_subnet.private_zone2.id
+    ]
+  }
+
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks]
+}
