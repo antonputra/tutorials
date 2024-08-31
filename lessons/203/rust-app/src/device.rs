5aa6631102eed91d51b15e8bc9cead55b54d9f87
Divit <divitngoc@gmail.com>
2024-09-01 05:03:26 +0700

    Lesson 203: Remove String to Uuid conversion to closely resemble go (#252)
    * Remove string to uuid conversion
    
    On par to what go is doing
    
    * Remove Uuid from device.rs 
    
    Device Struct now more closely resembles go Device
    
    * Clean uuid import on Device

--- lessons/203/rust-app/src/device.rs
+++ lessons/203/rust-app/src/device.rs
@@ -1,9 +1,8 @@
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Device {
-    pub uuid: Uuid,
+    pub uuid: String,
     pub mac: String,
     pub firmware: String,
 }
