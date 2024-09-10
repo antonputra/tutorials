7d44c91fac99ee8167df5fb519f9d8f7bd4f7e04
David Alsh <alshdavid@gmail.com>
2024-09-10 11:29:21 +1000

    fmt

--- lessons/205/rust-app/src/main.rs
+++ lessons/205/rust-app/src/main.rs
@@ -5,7 +5,12 @@ mod metrics;
 mod routes;
 mod state;
 
-use std::{future::ready, io, net::{SocketAddr, ToSocketAddrs}, time::Duration};
+use std::{
+    future::ready,
+    io,
+    net::{SocketAddr, ToSocketAddrs},
+    time::Duration,
+};
 
 use axum::{http::StatusCode, routing::get, Router};
 use metrics::setup_metrics_recorder;
@@ -56,11 +61,11 @@ async fn main() {
 
     // Bind the server to the configured port.
     let listener = match create_listener(format!("0.0.0.0:{}", port)) {
-      Ok(listener) => listener,
-      Err(err) => {
-          eprintln!("Failed to bind to port {}: {:?}", port, err);
-          return;
-      }
+        Ok(listener) => listener,
+        Err(err) => {
+            eprintln!("Failed to bind to port {}: {:?}", port, err);
+            return;
+        }
     };
 
     // Start the server.
@@ -70,21 +75,21 @@ async fn main() {
 }
 
 fn create_listener<A: ToSocketAddrs>(addr: A) -> io::Result<tokio::net::TcpListener> {
-  let mut addrs = addr.to_socket_addrs()?;
-  let addr = addrs.next().unwrap();
-  let listener = match &addr {
-    SocketAddr::V4(_) => Socket::new(Domain::IPV4, Type::STREAM, None)?,
-    SocketAddr::V6(_) => Socket::new(Domain::IPV6, Type::STREAM, None)?,
-  };
+    let mut addrs = addr.to_socket_addrs()?;
+    let addr = addrs.next().unwrap();
+    let listener = match &addr {
+        SocketAddr::V4(_) => Socket::new(Domain::IPV4, Type::STREAM, None)?,
+        SocketAddr::V6(_) => Socket::new(Domain::IPV6, Type::STREAM, None)?,
+    };
 
-  listener.set_nonblocking(true)?;
-  listener.set_nodelay(true)?;
-  listener.set_reuse_address(true)?;
-  listener.set_linger(Some(Duration::from_secs(0)))?;
-  listener.bind(&addr.into())?;
-  listener.listen(i32::MAX)?;
+    listener.set_nonblocking(true)?;
+    listener.set_nodelay(true)?;
+    listener.set_reuse_address(true)?;
+    listener.set_linger(Some(Duration::from_secs(0)))?;
+    listener.bind(&addr.into())?;
+    listener.listen(i32::MAX)?;
 
-  let listener = std::net::TcpListener::from(listener);
-  let listener = tokio::net::TcpListener::from_std(listener)?;
-  Ok(listener)
+    let listener = std::net::TcpListener::from(listener);
+    let listener = tokio::net::TcpListener::from_std(listener)?;
+    Ok(listener)
 }
