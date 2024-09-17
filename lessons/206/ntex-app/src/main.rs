dc8648fa5a4b6094f97791119accd4cb282f096f
Gerard Zinn <gerardjoven2020@hotmail.com>
2024-09-17 17:46:00 +0200

    Added Ntex and rust optimizations

---
+++ lessons/206/ntex-app/src/main.rs
@@ -0,0 +1,13 @@
+use ntex::web::{self, App};
+use routes::{devices, health};
+
+mod device;
+mod routes;
+
+#[ntex::main]
+async fn main() -> std::io::Result<()> {
+    web::server(|| App::new().service((health, devices)))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
+}
