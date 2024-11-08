adaffbcdf78a7f1f8a8e74f38b154b4574e328c8
Yordis Prieto <yordis.prieto@gmail.com>
2024-11-07 21:43:47 -0500

    feat: add elixir with bandit

---
+++ lessons/222/elixir-app/README.md
@@ -0,0 +1,8 @@
+# App
+
+To start your Bandit server:
+
+- Run `mix setup` to install and setup dependencies
+- Run `iex -S mix` to start the server
+
+Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
---
+++ lessons/222/elixir-app/config/runtime.exs
@@ -0,0 +1,24 @@
+import Config
+
+if config_env() == :prod do
+  database_url =
+    System.get_env("DATABASE_URL") ||
+      raise """
+      environment variable DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
+
+  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
+
+  config :app, App.Repo,
+    # ssl: true,
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("REPO_POOL_SIZE", "10")),
+    queue_target: String.to_integer(System.get_env("REPO_QUEUE_TARGET", "50")),
+    queue_interval: String.to_integer(System.get_env("REPO_QUEUE_INTERVAL", "1000")),
+    socket_options: maybe_ipv6
+
+  config :app, App.Router,
+    serve: System.get_env("WEB_SERVER", "false") == "true",
+    port: String.to_integer(System.get_env("WEB_PORT", "4000"))
+end
