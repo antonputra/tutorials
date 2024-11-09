d2314e7eb41e7525173e5940c4ea1c2d4424ecba
OreQr <50458706+OreQr@users.noreply.github.com>
2024-11-09 20:51:06 +0100

    Switch node:http to uWS C++
    µWebSockets.js is fast Node.js web server written in C++
    
    Ruby was benchmarked with third-patry web server also written in C
    
    Also downgrade node version to v22.11.0 because uWS doesn't support node 23
    
    NOTICE: I didin't benchmark it locally but it's supposed to work.

--- lessons/222/node-app/app.js
+++ lessons/222/node-app/app.js
@@ -4,94 +4,137 @@ import { randomUUID } from "crypto";
 import { register } from "prom-client";
 import config from "./config.js";
 
-import * as http from "node:http";
+import uWS from "uWebSockets.js";
 
-// Timeout in milliseconds
-const server = http.createServer({ keepAliveTimeout: 60000 }, (req, res) => {
-  if (req.url === "/metrics") {
-    res.writeHead(200, { "Content-Type": register.contentType });
-    register.metrics().then((data) => res.end(data));
-    return;
-  }
+const app = uWS.App();
 
-  if (req.url === "/healthz") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("OK");
-    return;
-  }
-
-  if (req.method === "GET" && req.url === "/api/devices") {
-    const devices = [
-      {
-        id: 1,
-        uuid: "9add349c-c35c-4d32-ab0f-53da1ba40a2a",
-        mac: "5F-33-CC-1F-43-82",
-        firmware: "2.1.6",
-        created_at: "2024-05-28T15:21:51.137Z",
-        updated_at: "2024-05-28T15:21:51.137Z",
-      },
-      {
-        id: 2,
-        uuid: "d2293412-36eb-46e7-9231-af7e9249fffe",
-        mac: "E7-34-96-33-0C-4C",
-        firmware: "1.0.3",
-        created_at: "2024-01-28T15:20:51.137Z",
-        updated_at: "2024-01-28T15:20:51.137Z",
-      },
-      {
-        id: 3,
-        uuid: "eee58ca8-ca51-47a5-ab48-163fd0e44b77",
-        mac: "68-93-9B-B5-33-B9",
-        firmware: "4.3.1",
-        created_at: "2024-08-28T15:18:21.137Z",
-        updated_at: "2024-08-28T15:18:21.137Z",
-      },
-    ];
-
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(devices));
-    return;
-  }
-
-  if (req.method === "POST" && req.url === "/api/devices") {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on("end", () => {
-      let device = JSON.parse(body);
-      var datetime = new Date();
-
-      device.uuid = randomUUID();
-      device.created_at = datetime;
-      device.updated_at = datetime;
-
-      const end = histogram.startTimer();
-      save(device)
-        .then((record) => {
-          end({ op: "db" });
-
-          device.id = record[0].id;
-
-          res.writeHead(201, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(device));
-        })
-        .catch((error) => {
-          console.error(error);
-
-          res.writeHead(400, { "Content-Type": "application/json" });
-          res.end(JSON.stringify({ message: error.message }));
-        });
-    });
-
-    return;
-  }
-
-  res.writeHead(404, { "Content-Type": "text/plain" });
-  res.end("Not Found");
+app.get("/metrics", (res) => {
+  res.writeHeader("Content-Type", register.contentType);
+  register.metrics().then((data) => res.end(data));
 });
 
-console.log(`Node is listening on http://0.0.0.0:${config.appPort} ...`);
+app.get("/healthz", (res) => {
+  res.writeHeader("Content-Type", "text/plain").end("OK");
+});
 
-server.listen(config.appPort);
+app.get("/api/devices", (res) => {
+  const devices = [
+    {
+      id: 1,
+      uuid: "9add349c-c35c-4d32-ab0f-53da1ba40a2a",
+      mac: "5F-33-CC-1F-43-82",
+      firmware: "2.1.6",
+      created_at: "2024-05-28T15:21:51.137Z",
+      updated_at: "2024-05-28T15:21:51.137Z",
+    },
+    {
+      id: 2,
+      uuid: "d2293412-36eb-46e7-9231-af7e9249fffe",
+      mac: "E7-34-96-33-0C-4C",
+      firmware: "1.0.3",
+      created_at: "2024-01-28T15:20:51.137Z",
+      updated_at: "2024-01-28T15:20:51.137Z",
+    },
+    {
+      id: 3,
+      uuid: "eee58ca8-ca51-47a5-ab48-163fd0e44b77",
+      mac: "68-93-9B-B5-33-B9",
+      firmware: "4.3.1",
+      created_at: "2024-08-28T15:18:21.137Z",
+      updated_at: "2024-08-28T15:18:21.137Z",
+    },
+  ];
+
+  res
+    .writeHeader("Content-Type", "application/json")
+    .end(JSON.stringify(devices));
+});
+
+app.post("/api/devices", (res) => {
+  readJson(res, (device) => {
+    const datetime = new Date();
+
+    device.uuid = randomUUID();
+    device.created_at = datetime;
+    device.updated_at = datetime;
+
+    const end = histogram.startTimer();
+    save(device)
+      .then((record) => {
+        end({ op: "db" });
+
+        device.id = record[0].id;
+
+        res.cork(() => {
+          res
+            .writeStatus("201")
+            .writeHeader("Content-Type", "application/json")
+            .end(JSON.stringify(device));
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+
+        res.cork(() => {
+          res
+            .writeStatus("400")
+            .writeHeader("Content-Type", "application/json")
+            .end(JSON.stringify({ message: error.message }));
+        });
+      });
+  });
+});
+
+app.any("/*", (res) => {
+  res
+    .writeStatus("404")
+    .writeHeader("Content-Type", "text/plain")
+    .end("Not Found");
+});
+
+app.listen(config.appPort, (token) => {
+  if (token) {
+    console.log(`Node is listening on http://0.0.0.0:${config.appPort} ...`);
+  }
+});
+
+// Source: https://github.com/uNetworking/uWebSockets.js/blob/master/examples/JsonPost.js
+/* Helper function for reading a posted JSON body */
+function readJson(res, cb, err) {
+  let buffer;
+  /* Register data cb */
+  res.onData((ab, isLast) => {
+    let chunk = Buffer.from(ab);
+    if (isLast) {
+      let json;
+      if (buffer) {
+        try {
+          json = JSON.parse(Buffer.concat([buffer, chunk]));
+        } catch (e) {
+          /* res.close calls onAborted */
+          res.close();
+          return;
+        }
+        cb(json);
+      } else {
+        try {
+          json = JSON.parse(chunk);
+        } catch (e) {
+          /* res.close calls onAborted */
+          res.close();
+          return;
+        }
+        cb(json);
+      }
+    } else {
+      if (buffer) {
+        buffer = Buffer.concat([buffer, chunk]);
+      } else {
+        buffer = Buffer.concat([chunk]);
+      }
+    }
+  });
+
+  /* Register error cb */
+  res.onAborted(err);
+}
