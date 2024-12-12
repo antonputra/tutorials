1aecbb95b6ae38146bc68e211767345c40215ed5
Aloizio Macedo <aloiziomacedo@gmail.com>
2024-12-12 12:22:53 -0300

    Use asyncpg in Python

--- lessons/231/python-app/db.py
+++ lessons/231/python-app/db.py
@@ -2,19 +2,20 @@ import os
 
 from typing import Annotated
 from fastapi import Depends
-from sqlmodel import Session, create_engine
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 
-POSTGRES_URI = os.environ['POSTGRES_URI']
-POSTGRES_POOL_SIZE = int(os.environ['POSTGRES_POOL_SIZE'])
+POSTGRES_URI = os.environ["POSTGRES_URI"]
+POSTGRES_POOL_SIZE = int(os.environ["POSTGRES_POOL_SIZE"])
 
 
-engine = create_engine(POSTGRES_URI, pool_size=POSTGRES_POOL_SIZE)
+engine = create_async_engine(POSTGRES_URI, pool_size=POSTGRES_POOL_SIZE)
 
 
-def get_postgres_session():
-    with Session(engine) as session:
+async def get_postgres_session():
+    async with AsyncSession(engine) as session:
         yield session
 
 
-PostgresDep = Annotated[Session, Depends(get_postgres_session)]
+PostgresDep = Annotated[AsyncSession, Depends(get_postgres_session)]
