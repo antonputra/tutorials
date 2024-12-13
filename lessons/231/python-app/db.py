9c7e54dc085b1d05dc42a1e2799e16e7ed7c97ff
Uday Mungalpara <techudaymungalpara@gmail.com>
2024-12-13 18:04:18 +0530

    refactor: replace SQLModel with SQLAlchemy - Remove SQLModel since it uses SQLAlchemy under the hood - Add new SQLAlchemy models for database

--- lessons/231/python-app/db.py
+++ lessons/231/python-app/db.py
@@ -1,20 +1,26 @@
 import os
-
 from typing import Annotated
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlmodel.ext.asyncio.session import AsyncSession
 
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 POSTGRES_URI = os.environ["POSTGRES_URI"]
 POSTGRES_POOL_SIZE = int(os.environ["POSTGRES_POOL_SIZE"])
 
 
-engine = create_async_engine(POSTGRES_URI, pool_size=POSTGRES_POOL_SIZE)
+engine = create_async_engine(
+    POSTGRES_URI,
+    echo=False,
+    pool_pre_ping=False,
+    pool_size=POSTGRES_POOL_SIZE,
+    max_overflow=0,
+)
+
+async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def get_postgres_session():
-    async with AsyncSession(engine) as session:
+    async with async_session() as session:
         yield session
 
 
