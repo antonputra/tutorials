3e536ba5bc3913ee7248fa11ecfbc20eeceb2a2f
Jorge López <jorgekq@hotmail.es>
2025-02-11 12:07:31 +0100

    Reduce the size of non-changing strings

--- lessons/244/rust-app/src/device.rs
+++ lessons/244/rust-app/src/device.rs
@@ -3,6 +3,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Device {
     pub id: Option<i32>,
-    pub mac: String,
-    pub firmware: String,
+    pub mac: Box<str>,
+    pub firmware: Box<str>,
 }
