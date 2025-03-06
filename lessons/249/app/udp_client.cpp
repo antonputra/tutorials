6fe13e2b9e1929f8c1093c187b274ff4e9a3a511
The-Emperor10 <44948520+The-Emperor10@users.noreply.github.com>
2025-03-06 01:39:22 -0500

    249 TCP vs UDP: optimize (#444)

--- lessons/249/app/udp_client.cpp
+++ lessons/249/app/udp_client.cpp
@@ -6,33 +6,28 @@
 #include "monitoring.hpp"
 
 using std::string;
-using std::to_string;
-using std::chrono::duration;
-using std::chrono::microseconds;
 using std::chrono::system_clock;
 
 #define PORT "8080"
-#define ADDRESS "udp-server.antonputra.pvt"
+//#define ADDRESS "udp-server.antonputra.pvt"
+#define ADDRESS "localhost"
 
+using namespace std::literals;
 // Declare some variables for the test.
 int stage_count = 1;
-int stage_interval_s = 0;
-int sleep_us = 1;
+auto stage_interval = 0s;
+auto sleep_time = 1us;
 
 int main()
 {
     const char *enable_monitoring_env = std::getenv("ENABLE_MONITORING");
-
-    std::string enable_monitoring(enable_monitoring_env);
+    bool enable_monitoring = enable_monitoring_env && std::strcmp(enable_monitoring_env, "true") == 0;
 
     // This variable hold file descriptor. We use them to read and write data.
     int client_fd, bytes_send;
 
     // addrinfo is used to prepare the socket address structures.
     struct addrinfo hints, *servinfo;
-
-    // This variable is used to hold the client IP address.
-    struct sockaddr_storage client_addr;
 
     // Make sure the struct is empty.
     memset(&hints, 0, sizeof hints);
@@ -47,12 +42,30 @@ int main()
     hints.ai_flags = AI_PASSIVE;
 
     // Fill out the struct. Assign the address of of the server to the socket structure, etc.
-    getaddrinfo(ADDRESS, PORT, &hints, &servinfo);
+    int err;
+    if ((err = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
+    {
+        std::cerr << "getaddrinfo failed: " << gai_strerror(err) << std::endl;
+        return 1;
+    }
 
     // Create a socket for the client and return a file descriptor that we can use to send data.
     client_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
 
     string msg = "{\"id\":66009,\"mac\":\"81-6E-79-DA-5A-B2\",\"firmware\":\"4.0.2\",\"create_at\": 170341283583666}";
+
+    // Batch messages together. This helps UDP more than TCP as the kernel will do this automatically
+    // for TCP unless TCP_NODELAY is set. TCP will still be helped by the reduced number
+    // of syscalls and context switches.
+    msg.reserve(UDP_BUFSIZE);
+    if (!enable_monitoring)
+    {
+        intptr_t batch_count = UDP_BUFSIZE / msg.length() - 1;
+        for(; batch_count > 0; --batch_count)
+        {
+            msg += "{\"id\":66009,\"mac\":\"81-6E-79-DA-5A-B2\",\"firmware\":\"4.0.2\",\"create_at\": 170341283583666}";
+        }
+    }
 
     for (int stage = 0; stage < stage_count; stage++)
     {
@@ -63,32 +76,38 @@ int main()
         while (true)
         {
             // Sleep to add a delay between sending messages to the client.
-            std::this_thread::sleep_for(microseconds(sleep_us));
+            std::this_thread::sleep_for(sleep_time);
 
-            if (enable_monitoring == "true")
+            if (enable_monitoring)
             {
                 // Generate a payload message with a timestamp to measure the duration.
-                msg = monitoring::generate_payload();
+                msg.clear();
+                while(msg.length() + MAXDATASIZE < UDP_BUFSIZE)
+                {
+                    char buf[MAXDATASIZE];
+                    monitoring::generate_payload(buf);
+                    msg += buf;
+                }
             }
 
             // Send data to UDP server. Returns -1 on error.
-            bytes_send = sendto(client_fd, msg.c_str(), msg.length(), 0, servinfo->ai_addr, servinfo->ai_addrlen);
+            bytes_send = sendto(client_fd, msg.data(), msg.length(), 0, servinfo->ai_addr, servinfo->ai_addrlen);
 
             // Check if we successfully send data ro server.
             if (bytes_send == -1)
             {
-                std::cout << "failed to send a message to the server." << std::endl;
-                exit(1);
+                perror("failed to send a message to the server: ");
+                return 1;
             }
 
             // Get the current time.
-            duration<double> elapsed_seconds = system_clock::now() - start_time;
+            auto elapsed_time = system_clock::now() - start_time;
 
             // Finish the state based on the time elapsed.
-            if (elapsed_seconds.count() > stage_interval_s)
+            if (elapsed_time > stage_interval)
             {
-                sleep_us -= 1;
-                std::cout << to_string(stage) + " stage is finished, sleeping now for: " + to_string(sleep_us) + " microseconds" << std::endl;
+                sleep_time -= 1us;
+                std::cout << stage << " stage is finished, sleeping now for: " << sleep_time.count() << " microseconds" << std::endl;
                 break;
             }
         }
@@ -98,10 +117,16 @@ int main()
     std::cout << "Starting the final stage." << std::endl;
     while (true)
     {
-        if (enable_monitoring == "true")
+        if (enable_monitoring)
         {
             // Generate a payload message with a timestamp to measure the duration.
-            msg = monitoring::generate_payload();
+            msg.clear();
+            while(msg.length() + MAXDATASIZE < UDP_BUFSIZE)
+            {
+              char buf[MAXDATASIZE];
+              monitoring::generate_payload(buf);
+              msg += buf;
+            }
         }
 
         // Send data to UDP server. Returns -1 on error.
@@ -111,7 +136,7 @@ int main()
         if (bytes_send == -1)
         {
             std::cout << "failed to send a message to the server." << std::endl;
-            exit(1);
+            return 1;
         }
     }
 
