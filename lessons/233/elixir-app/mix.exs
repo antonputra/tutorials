ab5e9020db7d2897848b666df28bd5f7ec8c11ea
Astolfo <git@astolfo.dev>
2025-03-14 15:27:38 +0300

    Update elixir

--- lessons/233/elixir-app/mix.exs
+++ lessons/233/elixir-app/mix.exs
@@ -5,7 +5,7 @@ defmodule App.MixProject do
     [
       app: :app,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
