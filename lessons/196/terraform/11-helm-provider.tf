1a5f5cfcc861c7d45965f291b9fa43dc00ca36ff
Lakmal Niranga <lakmalniranga@gmail.com>
2025-12-14 12:59:30 +0100

    fix: correct helm provider kubernetes block syntax

--- lessons/196/terraform/11-helm-provider.tf
+++ lessons/196/terraform/11-helm-provider.tf
@@ -7,7 +7,7 @@ data "aws_eks_cluster_auth" "eks" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = data.aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks.token
