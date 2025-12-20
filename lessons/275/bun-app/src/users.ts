58a0e726a783a3f43b996b43a6497f380eaed673
Solmanter <suleyman91002@gmail.com>
2025-12-21 00:58:21 +0500

    Update build script and refactor user saving logic
    - Modified the build script in package.json to include the --sql-preconnect option for improved database connection handling.
    - Refactored the save function in users.ts to destructure the user object, enhancing code readability and maintainability.

--- lessons/275/bun-app/src/users.ts
+++ lessons/275/bun-app/src/users.ts
@@ -1,6 +1,7 @@
 import sql from "./db.ts";
 import type { User } from "./types.ts";
 
-export async function save(user: User): Promise<[{ id: number }]> {
-  return sql`INSERT INTO bun_app (name, address, phone, image, created_at, updated_at) VALUES (${user.name}, ${user.address}, ${user.phone}, ${user.image}, ${user.createdAt}, ${user.updatedAt}) RETURNING id;`;
+export function save(user: User): Promise<[{ id: number }]> {
+  const { name, address, phone, image, createdAt, updatedAt } = user;
+  return sql`INSERT INTO bun_app (name, address, phone, image, created_at, updated_at) VALUES (${name}, ${address}, ${phone}, ${image}, ${createdAt}, ${updatedAt}) RETURNING id;`;
 }
